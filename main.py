f417c64b8958cac6c095356f3555d40bb2542c55
Archit <e1528239@u.nus.edu>
2026-05-09 14:04:00 +0800

    Render PDF into PIL images using pymupdf

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from pathlib import Path
 import fitz  # pymupdf
 from PIL import Image, ImageTk
 
-from config.config import APPLESCRIPT, TEMP_DIR, TEMP_PDF
+from config.config import APPLESCRIPT, TEMP_DIR, TEMP_PDF, PAGE_W
 
-# Control Applescript Execution
+# Control Applescript Execution ---------------------------------------------------------------------
 
 def run_applescript(output_folder, output_file):
     """
@@ -28,5 +28,33 @@ def run_applescript(output_folder, output_file):
         raise RuntimeError(f"AppleScript failed:\n{result.stderr}")
     
 
+# Render temp PDF into PIL images -------------------------------------------------------------------
+
+def render_pdf(pdf_path: Path, width: int) -> list[Image.Image]:
+    """Convert PDF pages to PIL images scaled to the given width."""
+    if not pdf_path.exists():
+        raise FileNotFoundError(f"PDF not found: {pdf_path}")
+
+    doc = fitz.open(str(pdf_path))
+    resized = []
+    for page in doc:
+        mat = fitz.Matrix(150/72, 150/72)   # 150 DPI
+        pix = page.get_pixmap(matrix=mat)
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        ratio  = width / img.width
+        height = int(img.height * ratio)
+        resized.append(img.resize((width, height), Image.LANCZOS))
+
+    return resized
+
+
+# All Component Tests -------------------------------------------------------------------------------
+def render_pdf_test(pdf_path: Path, width: int):
+    images = render_pdf(pdf_path, width)
+    for i, img in enumerate(images):
+        img.save(f"page_{i+1}.png")
+
+
+
 if __name__ == "__main__":
-    run_applescript(TEMP_DIR, TEMP_PDF)
+   render_pdf_test(TEMP_PDF, PAGE_W)
