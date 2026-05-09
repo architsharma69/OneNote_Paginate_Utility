58cb09a071ab30edecef478aec2618d8048ba3b9
Archit <e1528239@u.nus.edu>
2026-05-09 12:53:15 +0800

    Refactor Applescript

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ from pathlib import Path
 import fitz  # pymupdf
 from PIL import Image, ImageTk
 
-from config.config import *
+from config.config import APPLESCRIPT
 
+# Control Applescript Execution
+
+def run_applescript():
+    """
+    We want to run the applescript file which saves to temporary PDF
+    """
+    result = subprocess.run(
+        ["osascript", str(APPLESCRIPT)],
+        capture_output=True,
+        text=True
+    )
+
+    if result.returncode != 0:
+        raise RuntimeError(f"AppleScript failed:\n{result.stderr}")
