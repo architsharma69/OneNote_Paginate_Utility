289de3ce1da2c8e99f2f1fd3d91ea8cef57e5551
Archit <e1528239@u.nus.edu>
2026-05-09 13:26:45 +0800

    Allowed insertion of temp filepath through main.py script

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ from pathlib import Path
 import fitz  # pymupdf
 from PIL import Image, ImageTk
 
-from config.config import APPLESCRIPT
+from config.config import APPLESCRIPT, TEMP_DIR, TEMP_PDF
 
 # Control Applescript Execution
 
-def run_applescript():
+def run_applescript(output_folder, output_file):
     """
     We want to run the applescript file which saves to temporary PDF
+    Here we also inject the output directory and filename into the applescript
     """
+    # Create output_folder if necessary
+    Path.mkdir(output_folder, exist_ok=True)
+
     result = subprocess.run(
-        ["osascript", str(APPLESCRIPT)],
+        ["osascript", str(APPLESCRIPT), output_folder, output_file],
         capture_output=True,
         text=True
     )
 
     if result.returncode != 0:
         raise RuntimeError(f"AppleScript failed:\n{result.stderr}")
+    
+
+if __name__ == "__main__":
+    run_applescript(TEMP_DIR, TEMP_PDF)
